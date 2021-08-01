41cca00af484ea59baff6f34a79e33fdf0fec865
Andrey Georgiev <andrey.gueorguiev@gmail.com>
2021-08-01 16:52:16 +1200

    added the word collection and statistic interfaces

---
+++ Booster_WordStream/Controllers/WordStatistics/IWordStats.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace Booster_WordStream.Controllers.WordStatistics
+{
+    public interface IWordStats
+    {
+        void AddWord(string word_in);
+
+        void SetWordDict(ref Dictionary<string, int> word_dict);
+    }
+}
---
+++ Booster_WordStream/Models/IWordCollection.cs
@@ -0,0 +1,13 @@
+﻿using Booster_WordStream.Controllers.WordStatistics;
+using System.Collections.Generic;
+
+namespace Booster_WordStream.Models
+{
+    public interface IWordCollection
+    {
+        public void AddStat<T>(ref T word_stat) where T : IWordStats;
+        void AddString(string in_str);
+        void AddWord(string in_word);
+        void AddWords(List<string> word_data);
+    }
+}
